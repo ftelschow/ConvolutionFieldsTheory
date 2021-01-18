2d5d1269dd37a631a9dad93b940eb0940fa04687
Fabian Telschow <ftelschow@ucsd.edu>
2021-01-18 13:49:26 +0100

    function to be called from a bashscript

---
+++ Simulations/LKCsim_Article.m
@@ -0,0 +1,206 @@
+function [] = LKCsim_Article( fwhm_switch,...
+                              field_name,...
+                              mask_name,...
+                              D,...
+                              Msim,...
+                              out )
+%--------------------------------------------------------------------------
+% ARGUMENTS
+% Mandatory
+%   fwhm_switch  logical true means that fwhm is fixed in this simulation,
+%                false means that Nsubj is fixed
+%   field_name   either "stat" or "nonstat"
+%   mask_name    either "box" or "sphere"
+%   D            dimension of the domain
+% Optional
+%   Msim         number of MC runs. Default 1e3.
+%   out          
+%
+%--------------------------------------------------------------------------
+% OUTPUT
+% obj  an object of class Fields representing white noise, which is not
+%      masked. 
+%
+%--------------------------------------------------------------------------
+%% Check input
+%--------------------------------------------------------------------------
+
+if ~exist( 'out', 'var' )
+    out = "";
+end
+
+if isnumeric( out )
+    out = num2str( out );
+end
+
+% Amount of Monte Carlo simulations
+if ~exist( 'Msim', 'var' )
+    Msim = 1e3;
+end
+
+%--------------------------------------------------------------------------
+%% Prepare workspace
+%--------------------------------------------------------------------------
+
+server = ~true;
+
+%----- BEGIN Change the next lines to make it run with your local installation!
+if ~server
+    % Add the RFTtoolbox to working path
+    addpath( genpath("/home/drtea/matlabToolboxes/RFTtoolbox/") )
+
+    % Add path to simulation code
+    addpath( "/home/drtea/matlabToolboxes/ConvolutionFieldsTheory/Simulations" )
+
+    % Add path for results
+    path_wd = "/home/drtea/matlabToolboxes/ConvolutionFieldsTheory/Results/";
+    
+else
+    path_CFthy = '/vols/Scratch/ukbiobank/nichols/SelectiveInf/ConvolutionFieldsTheory/';
+    
+    % Add the RFTtoolbox to working path
+    addpath( genpath("/home/drtea/matlabToolboxes/RFTtoolbox/") )
+
+    % Add path to simulation code
+    addpath( [ path_CFthy, 'Simulations' ] )
+
+    % Add path for results
+    path_wd = [path_CFthy, 'Results'];
+end
+
+%----- END
+
+
+%--------------------------------------------------------------------------
+%% Parameters for the Simulations 
+%--------------------------------------------------------------------------
+switch D
+    case 1
+        % Length of the domain
+        T = 100;
+
+        % Dimension of domain
+        dim   = [ T 1 ];
+        
+        % Resolution for obtaining the true LKCs
+        theory_res = 21;
+        
+        % Get the masks
+        mask_box = true( dim );
+        mask_sphere = mask_box;
+        mask_sphere( [ 1:3 (T-3):T ] ) = 0;
+        mask_sphere = ~mask_sphere;
+        
+    case 2
+        % Length of the domain
+        T = 30;
+
+        % Dimension of domain
+        dim   = [ T T ];
+        
+        % Resolution for obtaining the true LKCs
+        theory_res = 7;
+        
+        % Get the masks
+        mask_box = true( dim );
+        mask_sphere = true( dim );
+        mask_sphere = bndry_voxels( logical( mask_sphere ), 'full' );
+        
+    case 3
+        % Length of the domain
+        T = 30;
+
+        % Dimension of domain
+        dim   = [ T T T ];
+        
+        % Resolution for obtaining the true LKCs
+        theory_res = 7;
+end
+
+% Amount of bootstrap replicates for bHPE
+Mboot = 1e3;
+
+% Vector for FWHM dependence
+FWHM = [ 1 1.5 2 3 4 5 6 ];
+
+% Vector for sample size dependence
+Nsubj = [ 20 50 100 150 ];
+
+% Vector for resadd dependence
+Resadd = [ 1 3 5 ];
+
+% Methods compared in this simulation 
+methods = struct( 'convE', true, 'bHPE', [ Mboot, 1 ], ...
+                  'kiebelE', 1, 'formanE', 1 );
+              
+% String for output
+outname = "Sim_LKCestims_";
+
+
+%--------------------------------------------------------------------------
+%% Simulations of LKCs of Random Fields
+%--------------------------------------------------------------------------
+
+% Name of scenario
+if strcmp( field_name, "stat" )
+    case_name = "_stationary";
+    % Field does not get masked before application of convolutions to prevent
+    % non-stationary effects caused by the boundary.
+    mask_lat = false;
+
+else
+    case_name = "_nonstationary";
+    % Field does not get masked before application of convolutions to prevent
+    % non-stationary effects caused by the boundary.
+    mask_lat = true;
+
+end
+
+if strcmp( mask_name, "box" )
+    case_name = strcat( case_name, "_box" );
+    % Get the mask
+    mask = mask_box;
+else
+    case_name = strcat( case_name, "_sphere" );
+    % Get the mask
+    mask = mask_sphere;
+end
+
+
+%%%%%% Dependence on sample size
+if fwhm_switch
+    fwhm = FWHM(4);
+    [ results, theoryL, sim_time ] = LKCsimulation( Msim,...
+                                                    Nsubj,...
+                                                    fwhm,...
+                                                    Resadd,...
+                                                    methods,...
+                                                    mask,...
+                                                    mask_lat,...
+                                                    theory_res );
+
+    save( strcat( path_wd, outname, '_D', num2str(D), case_name, '_fixedFWHM', out,'.mat' ),...
+                  'D', 'fwhm', 'methods', 'Nsubj', 'fwhm', 'Resadd',...
+                  'dim', 'Mboot', 'Msim', 'theoryL',...
+                  'results', 'sim_time' );     
+    clear fwhm results theoryL sim_time
+
+    
+%%%%%% Dependence on FWHM
+else
+    nsubj = Nsubj(2);
+    [ results, theoryL, sim_time ] = LKCsimulation( Msim,...
+                                                    nsubj,...
+                                                    FWHM,...
+                                                    Resadd,...
+                                                    methods,...
+                                                    mask,...
+                                                    mask_lat,...
+                                                    theory_res );
+
+    save( strcat( path_wd, outname, '_D', num2str(D), case_name, '_fixednsubj', out,'.mat' ),...
+                  'D', 'nsubj', 'methods', 'FWHM', 'Resadd',...
+                  'dim', 'Mboot', 'Msim', 'theoryL',...
+                  'results', 'sim_time' );
+    clear nsuj results theoryL sim_time mask mask_lat
+end

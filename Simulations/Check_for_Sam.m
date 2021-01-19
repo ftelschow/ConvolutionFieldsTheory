49838edab3a4b9759d78679682c48e53f8fbdae9
Fabian Telschow <ftelschow@ucsd.edu>
2021-01-19 17:13:57 +0100

    small code chunk to be run to test the parallize coding part

---
+++ Simulations/Check_for_Sam.m
@@ -0,0 +1,65 @@
+%% Short check of parallelization for Sam
+% If this runs through smoothly, I hope it will too on the server
+%--------------------------------------------------------------------------
+
+% Choose dimension of the domain
+D = 2; % 1; %
+
+% Number of runs by dimension
+Drun = [ 4, 6 ];
+
+% Loop over runs which will be parallel on the server
+tic
+for run = 1:Drun(D)
+    switch D
+        case 1
+            % Setting for the different runs
+            if run == 1
+                fwhm_switch = 0;
+                field_name  = "stationary";
+                mask_name   = "box";
+            elseif run == 2
+                fwhm_switch = 1;
+                field_name  = "stationary";
+                mask_name   = "box";
+            elseif run == 3
+                fwhm_switch = 0;
+                field_name  = "nonstationary";
+                mask_name   = "sphere";
+            elseif run == 4
+                fwhm_switch = 1;
+                field_name  = "nonstationary";
+                mask_name   = "sphere";
+            end
+        case 2
+            if run == 1
+                fwhm_switch = 0;
+                field_name  = "stationary";
+                mask_name   = "box";
+            elseif run == 2
+                fwhm_switch = 1;
+                field_name  = "stationary";
+                mask_name   = "box";
+            elseif run == 3
+                fwhm_switch = 0;
+                field_name  = "stationary";
+                mask_name   = "sphere";
+            elseif run == 4
+                fwhm_switch = 1;
+                field_name  = "stationary";
+                mask_name   = "sphere";
+            elseif run == 5
+                fwhm_switch = 0;
+                field_name  = "nonstationary";
+                mask_name   = "sphere";
+            elseif run == 6
+                fwhm_switch = 1;
+                field_name  = "nonstationary";
+                mask_name   = "sphere";
+            end
+    end
+
+    % call the function simulating the LKCs
+    LKCsim_Article( fwhm_switch, field_name, mask_name, D, 2 )
+end
+toc

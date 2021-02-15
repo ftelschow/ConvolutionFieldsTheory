c14302419d4a40155b5691c8026c095983082915
Fabian Telschow <ftelschow@ucsd.edu>
2021-02-15 15:11:40 +0100

    changed to work on server

--- Simulations/Parallelize_2D_sphere_nonstationary.sh
+++ Simulations/Parallelize_2D_sphere_nonstationary.sh
@@ -5,11 +5,11 @@
 # Loop over parallelisations
 for parallel_count in {1..5}
 do
-    matlab -nodesktop -nosplash -r "LKCsim_Article( 0, 'nonstationary', 'sphere', 2, 200, 'num2str($parallel_count)' ); exit" &
+    matlab -nodesktop -nosplash -r "LKCsim_Article( 0, 'nonstationary', 'sphere', 2, 200, $parallel_count ); exit" &
 done
 
 # Loop over parallelisations
 for parallel_count in {1..5}
 do
-    matlab -nodesktop -nosplash -r "LKCsim_Article( 1, 'nonstationary', 'sphere', 2, 200, 'num2str($parallel_count)' ); exit" &
+    matlab -nodesktop -nosplash -r "LKCsim_Article( 1, 'nonstationary', 'sphere', 2, 200, $parallel_count ); exit" &
 done

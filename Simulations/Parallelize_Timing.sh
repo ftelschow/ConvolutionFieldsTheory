185986b019a810faecf72ca2f85dfa5bc18b5a7c
Fabian <fabian.telschow@hu-berlin.de>
2023-01-25 14:56:51 +0100

    small error removed

--- Simulations/Parallelize_Timing.sh
+++ Simulations/Parallelize_Timing.sh
@@ -5,5 +5,5 @@
 # Loop over parallelisations
 for D in {1..3}
 do
-    matlab -nodesktop -nosplash -r "LKCsim_Article_Timing( $D, 100,  $D ); exit" &
+    matlab -nodesktop -nosplash -r "LKCsim_Article_Timing( $D,  $D ); exit" &
 done
